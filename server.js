646b8d47dd3742e5c95a6910f8fd86d2e5388d0d
Andrew Crites <acrites@mobiquityinc.com>
2013-12-23 10:17:55 -0500

    Adding module inclusion on server start

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path')
+var express = require("express"),
+    http = require("http"),
+    fs = require("fs"),
     env = process.env.NODE_ENV || "development",
     config = require("./config/config")[env]
 ;
+
+fs.readdirSync(__dirname + "/app/mod").forEach(function (file) {
+    if (~file.indexOf(".js")) {
+        require(__dirname + "/app/mod/" + file);
+    }
+});
 
 var app = express();
 
 require("./config/express")(app, config);
 require("./config/routes")(app);
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function(){
+    console.log("Express server listening on port " + app.get("port"));
 });
